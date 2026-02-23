6e2d4085b8779fceae7352be4da22f297437e880
Frida <frida_132@icloud.com>
2026-02-22 22:38:11 -0800

    Just added silly_joke.py

---
+++ silly_joke.py
@@ -0,0 +1,12 @@
+print("Intitializing...")
+print("Scanning....")
+print("Still Scanning...")
+print("Hacking the mainframe to find you a funny joke...")
+print("Almost there...")
+import random
+jokes = [
+    "What did the waiter tell the horse when he came in to the restaurant?\nWhy the long face!!q≧▽≦q",
+    "What's a computer's fav snack?\nComputer Chips!! \^o^/",
+    "3 Database SQL walked into a NoSQL bar...\nA little while later, they walked out....Because they couldn't find a table!!!"
+]
+print("Here's  your joke:\n", random.choice(jokes))
